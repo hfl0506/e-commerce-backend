0c6b21cb8c677524f03c3a6907fb1910131a92b6
ronnieli <lihinfei0506@gmail.com>
2022-01-18 23:49:15 +0800

    typo fix for add back entity, provider at auth module

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtService } from 'src/utils/jwt.utils';
+import { ValidateService } from 'src/utils/validate.utils';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
+import { AuthEntity, AuthSchema } from './auth.entity';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: AuthEntity.name,
+        schema: AuthSchema,
+      },
+    ]),
+    UserModule,
+  ],
   exports: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, ValidateService, JwtService],
 })
 export class AuthModule {}
